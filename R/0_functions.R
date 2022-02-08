fdca6746703e18e1ad02ea668228679442b0ca9e
FrancisFayolle <78230345+FrancisFayolle@users.noreply.github.com>
2022-02-08 17:05:41 -0500

    read data file

---
+++ R/0_functions.R
@@ -0,0 +1,16 @@
+# The following steps have been thought to integrally cover the data cleaning process of the RMRP 2022 database. 
+# It's use can be adapted to obtain general or country specific data.
+
+## Author: R4V IM Team, Francis Fayolle and James Léon-Dufour
+
+## 
+
+# 1_read_data
+# Mandatory step to obtain 5W data that will then be process in the following steps of the process
+# Options:source: "file": reads data from XLS file  "activityinfo": Activity Indo API download
+# Default: "activityinfo" 
+# write: "yes": saves files, "no": do not save
+# default: "no"
+source("R/1_read_data.R")
+read_data_2022(source = "activityinfo",
+               write = "no")
---
+++ R/1_read_data.R
@@ -0,0 +1,108 @@
+# Read data 2022
+# function
+
+read_data_2022 <- function(data, 
+                           source = "activityinfo",
+                           write ="no")
+{ 
+
+# Load packages
+library(activityinfo)
+library(tidyverse)
+library(readxl)
+library(writexl)
+# Credentials located in seperate file
+source("R/ai_credentials.R")
+
+# Get data from different sources
+
+  if(source == "activityinfo"){
+    df5W <- queryTable("cw1o8nbkx69lc9l3",
+                     "Country" = "cezj1rqkxeqrsy57.c8u26b8kxeqpy0k4",
+                     "Country Admin1" = "cezj1rqkxeqrsy57.c3ns3zikxeqq4h95",
+                     "Admin2" = "c89klrbkx6hp4j58.cs2esadkx6hkt7j6",
+                     "Appealing organisation Name" = "c5648gjkx69ra2v9.ckj5zamkumvyysv9",
+                     "Implementation Set up" = "ckjtet4kx69smeog",
+                     "Implementing partner Name" = "cflyi17kx69vdxei.ckj5zamkumvyysv9",
+                     "Month" = "clqgqrqkyueahma8",
+                     "Subsector" = "cq7t4s8kx6a7fyx3.cgdeh97ktn4sdek3s.cfvkmslkpy3tg94n",
+                     "Indicator" = "cq7t4s8kx6a7fyx3.cwkj9p4kteeh4ls5",
+                     "Activity Name" = "c3p669wkx6a7oyo4",
+                     "Activity Description" = "c8hxf50kx6a7vp65",
+                     "COVID 19 Situation" = "c3sg2p7kx6am3uk8",
+                     "RMRP Activity" = "cuf3og8kx6amylmf",
+                     "CVA" = "cbvqg4jkx6b1kii7",
+                     "Value (in USD)" = "clwkfmckx6b2msu9",
+                     "Delivery mechanism" = "cg3rikqkx6b3z1kf",
+                     "Quantity of output" = "cm6no26kx6b8fqoh",
+                     "Total monthly beneficiaries" = "cto1biukx6kwvnj4k",
+                     "New beneficiaries of the month" = "c43j49ikx6kxyyc4l",
+                     "Refugees and Migrants IN DESTINATION" = "cz3yof2kx6l024p4m",
+                     "Refugees and Migrants IN TRANSIT" = "c8kl5o2kx6l0jip4n",
+                     "Host Communities Beneficiaries" = "c5z8bvakx6l10d84o",
+                     "Refugees and Migrants PENDULARS" = "c72dmskkx6l1hl04p",
+                     "Colombian Returnees" = "cmoqhuckx6l4q9z4q",
+                     "Women under 18" = "cwrxeaekx6l63na4s",
+                     "Men under 18" = "ccx7xhekx6l6jnk4t",
+                     "Women above 18" = "c3l36n2kx6l70kp4u",
+                     "Men above 18" = "ctd27ackx6l7g814v",
+                     "Other under 18" = "ckjcuiokx6l9a504w",
+                     "Other above 18" = "cq4hs3skx6lggpj4x")
+  } else {
+    df5W <- read_excel("data/RMRP2022Activities.xlsx")
+  }
+  
+ # format column names for easier data processing
+  
+  colnames(df5W) <- c("Country",
+                      "Admin1",
+                      "Admin2",
+                      "Appealing_org",
+                      "Implementation",
+                      "Implementing_partner",
+                      "Month",
+                      "Subsector",
+                      "Indicator",
+                      "Activity_Name",
+                      "Activity_Description",
+                      "COVID19",
+                      "RMRPActivity",
+                      "CVA",
+                      "Value",
+                      "Delivery_mechanism",
+                      "Quantity_output",
+                      "Total_monthly",
+                      "New_beneficiaries",
+                      "IN_DESTINATION",
+                      "IN_TRANSIT",
+                      "Host_Communities",
+                      "PENDULARS",
+                      "Returnees",
+                      "Girls",
+                      "Boys",
+                      "Women",
+                      "Men",
+                      "Other_under",
+                      "Other_above")
+  
+# Short data wrangling for categorical and integer values
+  
+
+# Not recommended but if needed, chose to write the 5W as a xlsx file in repository
+
+  if(write == "yes"){
+    write_xlsx(df5W, "./data/Activities_5W_2022.xlsx")
+    } else {}
+  
+# Get other reference table used during the data quality check
+# Loaded from AI regardless the method for 5W used
+  dfadmin2 <- queryTable("cn6oysukx6hk2cn3",
+                   "Country" = "cnkb6jykxgdeemm4r.c8u26b8kxeqpy0k4",
+                   "Country Admin1" = "cnkb6jykxgdeemm4r.c3ns3zikxeqq4h95",
+                   "Admin2" = "cs2esadkx6hkt7j6")
+
+  dfindicator <- queryTable("c49gyhmktedz4uj2",
+                   "Code" = "cob8rivktedzp0f3",
+                   "Subsector" = "cgdeh97ktn4sdek3s.cfvkmslkpy3tg94n",
+                   "Indicator type" = "cprepl2ktk2l76a3")
+} 
