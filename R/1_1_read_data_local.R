4106db550ab45e56f7cf5ca50c1b5d7484c856f9
FrancisFayolle <78230345+FrancisFayolle@users.noreply.github.com>
2022-03-28 15:18:45 -0500

    Updates for Admin2file

--- R/1_1_read_data_local.R
+++ R/1_1_read_data_local.R
@@ -77,13 +77,11 @@ read_data_2022_local <- function(data)
     mutate(countryadmin1 = paste(Country, Admin1))%>%
     ungroup()
   
-  dfadmin2  <<- queryTable("cn6oysukx6hk2cn3",
-                           "Country" = "cnkb6jykxgdeemm4r.c8u26b8kxeqpy0k4",
-                           "Admin1" = "cnkb6jykxgdeemm4r.c3ns3zikxeqq4h95",
-                           "Admin2" = "cs2esadkx6hkt7j6", truncate.strings = FALSE)%>%
-    rowwise()%>%
-    mutate(admin1and2 = paste(Admin1, Admin2))%>%
-    ungroup()
+  dfadmin2 <<- read_excel("./docs/Admin2SHP.xlsx")%>%
+    select(Country, Admin1, Admin2)%>%
+     rowwise()%>%
+     mutate(admin1and2 = paste(Admin1, Admin2))%>%
+     ungroup()
   
   dfindicator  <<- queryTable("c49gyhmktedz4uj2",
                               "Code" = "cob8rivktedzp0f3",
